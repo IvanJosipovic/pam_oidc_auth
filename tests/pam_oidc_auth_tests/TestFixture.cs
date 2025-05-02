8603c1729666f6e973a1febb3c605e9b0f604a3e
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-05-02 11:25:06 -0700

    wip

--- tests/pam_oidc_auth_tests/TestFixture.cs
+++ tests/pam_oidc_auth_tests/TestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using Ductus.FluentDocker.Builders;
 using Ductus.FluentDocker.Services;
+using Ductus.FluentDocker.Services.Extensions;
 
 namespace pam_oidc_auth_tests;
 
@@ -16,12 +17,17 @@ public class TestFixture : IDisposable
             .RemoveOrphans()
             .AssumeComposeVersion(Ductus.FluentDocker.Model.Compose.ComposeVersion.V2)
             .ForceRecreate()
-            .WaitForHttp("oidc-server-mock", "http://oidc-server-mock:8080/.well-known/openid-configuration", continuation: (resp, cnt) => resp.Body.Contains("jwks_uri") ? 0 : 500)
+            //.WaitForPort("oidc-server-mock", "8080/tcp")
+            .WaitForHttp("server", "http://oidc-server-mock:8080/.well-known/openid-configuration", continuation: (resp, cnt) => resp.Body.Contains("jwks_uri") ? 0 : 500)
             .Build()
             .Start();
 
+        var cont = compositeService.Containers.First(x => x.Name == "server");
+        //cont.WaitForPort("8080/tcp");
+        //cont.WaitForHttp("http://oidc-server-mock:8080/.well-known/openid-configuration", continuation: (resp, cnt) => resp.Body.Contains("jwks_uri") ? 0 : 500);
+
         // Unsure why this is needed, but it is. The container is not ready until this delay is over.
-        Task.Delay(10000).Wait();
+        //Task.Delay(10000).Wait();
     }
 
     public INetworkService GetNetwork()
